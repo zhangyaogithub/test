1d160a7e6e24a7bbff08fb93a08e6dba1261ebd1
dreamer_github <812415556@qq.com>
2019-11-10 18:54:35 +0800

    增加csrf表单验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,11 +7,29 @@ use App\Models\User;
 
 class UsersController extends Controller
 {
+    /**
+     * 注册展示页面
+     * @return [type] [description]
+     */
     public function create(){
         return view('users.create');
     }
 
+    /**
+     * 个人中心
+     * @param  User   $user [description]
+     * @return [type]       [description]
+     */
     public function show(User $user){
         return view('users.show',compact('user'));
+    }
+
+    public function store(Request $request){
+        $this->validate($request,[
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6|max:18'
+        ]);
+        return;
     }
 }
